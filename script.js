cc9204dc3776ccaa66fe40b19d0db163422cb8b3
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-17 09:24:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 function firstNonRepeatedChar(str) {
-	let arr = new Array(256).fill(0);
-	for(let i = 0; i < str.length; i++){
-		let c = str.charCodeAt(i);
-		arr[c]++;
-	}
-	let res = -1;
-	let flag = true;
-	for(let i = 0; i < arr.length; i++){
-		if(arr[i] === 1){
-			res = i;
-			flag = false;
-			break;
-		}
-	}
-	 if (!flag) {
+    if (str.length === 0) {
+        return "null";
+    }
+    str = str.toLowerCase();
+    let arr = new Array(26).fill(0);
+    for (let i = 0; i < str.length; i++) {
+        let c = str.charCodeAt(i) - 97; // Adjust to 0-based index for lowercase letters
+        arr[c]++;
+    }
+    let res = -1;
+    let flag = true;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 1) {
+            res = i + 97; // Adjust back to ASCII code for lowercase letters
+            flag = false;
+            break;
+        }
+    }
+    if (!flag) {
         return String.fromCharCode(res);
     } else {
-        return "";
+        return "null";
     }
 }
 const input = prompt("Enter a string");
